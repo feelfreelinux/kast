e69b37f246b122bc269dda8e7f12d335204c87f9
rlot <rj23@protonmail.com>
2017-04-04 12:25:21 +0200

    Implemented HEAD request support in HTTPFileServer.
    Bug: #9

--- src/HTTPFileServer.cpp
+++ src/HTTPFileServer.cpp
@@ -30,22 +30,25 @@ void HttpFileServer::handleIncoming()
 
     QUrl filePath;
     QMap<QString, QString> requestMap;
-
+    QString requestType; // Request handled for now - GET and HEAD
     // Read client's request
     while(!clientConnection->atEnd())
     {
             QString line = clientConnection->readLine();
 
+            // Get type of request
+            if(line.startsWith("GET /")) requestType = "GET";
+            else if (line.startsWith("HEAD /")) requestType = "HEAD";
+
             // Get filename from request
-            if(line.startsWith("GET /"))
+            if(!requestType.isEmpty() && line.contains(requestType))
             {
-                line.replace("GET /", ""); // HTTP header left part
+                line.replace(requestType+" /", ""); // HTTP header left part
                 line.chop(11); // HTTP header right part
 
                 int id = line.left(line.lastIndexOf("/")).toInt();
                 QString fileName = line.right(line.length() - line.lastIndexOf("/") - 1);
                 filePath = fileMap[id];
-
                 // Ensures the file is valid
                 fileinfo.setFile(filePath.toString());
                 if(fileinfo.suffix() != line.split(".").last() ||
@@ -55,7 +58,7 @@ void HttpFileServer::handleIncoming()
                         !fileinfo.isReadable())
                 {
                     // If file is not valid, write 404 and return
-                    qDebug() << "File not valid: " + filePath.toString();
+                    qDebug() << "File not valid: " + fileName;
                     clientConnection->write("HTTP/1.0 404 NOT FOUND\r\n");
                     clientConnection->flush();
                     clientConnection->disconnectFromHost();
@@ -70,31 +73,42 @@ void HttpFileServer::handleIncoming()
                     value(line.mid(line.indexOf(":") + 2, line.length()));
 
             requestMap.insert(key, value);
+
     }
     // If no error occured, send and process file
-    if(!filePath.isEmpty())
+    if(!filePath.isEmpty() && !requestType.isEmpty())
     {
             QFile file(filePath.toString());
             file.open(QFile::ReadOnly);
             QString header, filesize = QString::number(file.size());
 
             // Detect, does Range header exist in request
-            if(requestMap.contains("Range"))
+            if(requestMap.contains("Range") && requestType=="GET")
             {
                 // Parse request, get seek value
                 QString range = requestMap["Range"];
                 range = range.mid(6, range.length()); // 'bytes=' is 6 chars
                 qint64 seek = range.left(range.indexOf("-")).toInt();
-                if(range.endsWith("-"))
-                    range.append(QString::number(file.size() - 1));
-                // Construct 206 header, with content lenght
-                header = QString("HTTP/1.0 206 PARTIAL CONTENT\r\n"
-                         "Content-Length: %1\r\n"
-                         "Content-Range: bytes %2/%1\r\n"
-                         "Content-Type: %3\r\n\r\n").arg(filesize, range,
-                                                         db.mimeTypeForFile(fileinfo).name());
-                // Finally, seek the file
-                file.seek(seek);
+                // Check, is requested filesize acceptable
+                if(seek<=filesize.toInt()) {
+                    if(range.endsWith("-"))
+                        range.append(QString::number(file.size() - 1));
+                    // Construct 206 header, with content lenght
+                    header = QString("HTTP/1.0 206 PARTIAL CONTENT\r\n"
+                            "Content-Length: %1\r\n"
+                            "Content-Range: bytes %2/%1\r\n"
+                            "Content-Type: %3\r\n\r\n").arg(filesize, range,
+                                                            db.mimeTypeForFile(fileinfo).name());
+                    // Finally, seek the file
+                    file.seek(seek);
+                } else { // Handle 416 Range Not Satisfiable
+                    requestType = "FOOBAR"; // Break data sending
+                    // Construct 416
+                    header = QString("HTTP/1.0 416 Range Not Satisfiable\r\n"
+                             "Content-Length: %1\r\n"
+                             "Content-Type: %2\r\n\r\n").arg(filesize,
+                                                             db.mimeTypeForFile(fileinfo).name());
+                }
             }
             else
                 // If Range header is not detected, construct 200 OK header
@@ -105,16 +119,18 @@ void HttpFileServer::handleIncoming()
             // Write header to client
             clientConnection->write(header.toUtf8());
             clientConnection->waitForBytesWritten();
-            QByteArray block; block.resize(65536);
+            // If type is GET, send data of file.
+            if(requestType=="GET") {
+                QByteArray block; block.resize(65536);
+                    while(!file.atEnd())
+                    {
+                        // Send part of file to client
+                        qint64 read = file.read(block.data(), 65536);
+                        clientConnection->write(block, read);
+                    }
 
-            while(!file.atEnd())
-            {
-                // Send part of file to client
-                qint64 read = file.read(block.data(), 65536);
-                clientConnection->write(block, read);
-            }
-
-            file.close();
+                    file.close();
+                }
         }
         else
             // Send 404 if path is not valid
